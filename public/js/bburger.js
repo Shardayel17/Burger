c69e218615049d7b289b83262178b20b6255b82d
Shardayel17 <Shardayel17@yahoo.com>
2019-01-12 21:41:40 -0500

    public updates

--- public/js/bburger.js
+++ public/js/bburger.js
@@ -21,4 +21,42 @@ $(function() {
             );
         });
 
-        
+        $(".delete-burger").on("click", function(event) {
+            var id = $(this).data("id");
+    
+     
+    
+            $.ajax("/api/burgers/" + id, {
+              type: "DELETE",
+            }).then(
+    
+              function() {
+                console.log("deleted burger", id);
+                location.reload();
+              }
+            );
+          });
+    
+        $(".change-devour").on("click", function(event) {
+            var id = $(this).data("id");
+            var newDevour = $(this).data("newdevour");
+            var newDevourState = {
+            devoured: "true"
+            };
+    
+    
+    
+            $.ajax("/api/burgers/" + id, {
+              type: "PUT",
+              data: newDevourState
+    
+            }).then(
+    
+              function() {
+                console.log("changed devour to", newDevour);
+                location.reload();
+    
+              }
+            );
+          });   
+    });
