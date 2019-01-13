f87acf314d9bfacf119cfba4238bd5e7f54b0307
Shardayel17 <Shardayel17@yahoo.com>
2019-01-12 21:46:38 -0500

    hbars

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
 var express = require("express");
 
-
-
 var PORT = process.env.PORT || 8080;
-
-
 var app = express();
-
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
 var exphbs = require("express-handlebars");
-
-
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
 
-var routes = require("./controllers/burgers_controller.js");
-
+var routes = require("./controllers/controllers.js");
 
 
 app.use(routes);
